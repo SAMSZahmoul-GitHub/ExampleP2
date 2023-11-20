80828d3d941fe0c6ad15b5154542a49783dbe053
Rudra Kumar <74744301+rudraforweb@users.noreply.github.com>
2023-11-19 21:47:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
-# ---------------------------------------------------------------------------- #
-#                                                                              #
-# 	Module:       main.py                                                      #
-# 	Author:       rkumar                                                       #
-# 	Created:      10/21/2023, 10:45:44 PM                                       #
-# 	Description:  IQ2 project                                                  #
-#                                                                              #
-# ---------------------------------------------------------------------------- #
-
-# Library imports
-from vex import *
-
-# Brain should be defined by default
-brain=Brain()
-leftmotor=Motor(Ports.PORT7, False)
-leftmotor.spin_for(FORWARD, 455, DEGREES)
+# Hello. Type your name in the print statement and commit.
+print("")
